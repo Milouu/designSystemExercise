35e9b64dce06fdf4f7cbac5f7c7ffe247039b334
lihbr <lihbr@users.noreply.github.com>
2020-03-23 16:47:44 +0100

    feat: remove nasty btn hover

--- src/components/controls/Button.vue
+++ src/components/controls/Button.vue
@@ -3,10 +3,6 @@
     class="button py-2 px-4 rounded leading-5 border border-blue focus:outline-none"
     :class="{
       'text-blue': outline,
-      'hover:bg-blue': outline,
-      'hover:text-white': outline,
-      'hover:bg-white': !outline,
-      'hover:text-blue': !outline,
       'bg-blue': !outline,
       'text-white': !outline
     }"

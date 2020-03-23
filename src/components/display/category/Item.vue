8d78260faaa1a7f3dc190d3bcb3e7596863fb6a1
lihbr <lihbr@users.noreply.github.com>
2020-03-23 16:18:27 +0100

    fix: padding on categoryItem

--- src/components/display/category/Item.vue
+++ src/components/display/category/Item.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="categoryItem p-2 my-1 paragraph-alt flex items-center rounded -mx-2 hover:bg-grey-200 cursor-pointer"
+    class="categoryItem py-1 px-2 my-1 paragraph-alt flex items-center rounded -mx-2 hover:bg-grey-200 cursor-pointer"
     :class="{ 'bg-grey-200': active }"
   >
-    <div class="w-6 h-6 mr-2 flex items-center justify-center">
+    <div class="w-6 h-6 mr-2 flex items-center justify-center my-px">
       <slot name="icon" />
     </div>
     <slot />
